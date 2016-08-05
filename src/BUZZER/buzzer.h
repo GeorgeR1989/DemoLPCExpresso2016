9b09a77a63cc2e443c4b9e4d3ccfd2947650eb91
Ioan Bobar <ioan.bobar@ebs.msft>
2016-08-05 18:06:05 +0300

    Draft version 05.08_Stilling work

---
+++ src/BUZZER/buzzer.h
@@ -0,0 +1,32 @@
+
+#define P1_2_HIGH() (LPC_GPIO1->DATA |= (0x1<<2))
+#define P1_2_LOW()  (LPC_GPIO1->DATA &= ~(0x1<<2))
+
+
+static uint32_t notes[] = {
+        2272, // A - 440 Hz
+        2024, // B - 494 Hz
+        3816, // C - 262 Hz
+        3401, // D - 294 Hz
+        3030, // E - 330 Hz
+        2865, // F - 349 Hz
+        2551, // G - 392 Hz
+        1136, // a - 880 Hz
+        1012, // b - 988 Hz
+        1912, // c - 523 Hz
+        1703, // d - 587 Hz
+        1517, // e - 659 Hz
+        1432, // f - 698 Hz
+        1275, // g - 784 Hz
+};
+
+
+
+void delayMS(uint32_t delayInMs);
+void playNote(uint32_t, uint32_t );
+void playNote2(uint32_t note, uint32_t durationMs);
+void initPWM(uint16_t);
+void startPWM(void);
+void stopPWM(void);
+void updatePWM( uint8_t, uint8_t);
+
