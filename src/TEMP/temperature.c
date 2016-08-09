c81dbf1e1f16729a740f9589f0db9ab56ca6654d
Florin Brumar <florin.brumar@ebs.msft>
2016-08-09 18:14:57 +0300

    Temperature module, source file updated

--- src/TEMP/temperature.c
+++ src/TEMP/temperature.c
@@ -1,11 +1,7 @@
 /*
-===============================================================================
- Name        : temperature.c
- Author      : $(author)
- Version     :
- Copyright   : $(copyright)
- Description : main definition
-===============================================================================
+File name: temperature.c
+Date:8/4/2016
+Description: changing the unit for the displayed temperature (Celsius or Fahreinheit)
 */
 
 #include "temperature.h"
@@ -13,106 +9,72 @@
 /*..........................................................................................*/
 
 
-                  /*           Local variables                     */
+                 /*           Local variables                     */
 
-
-volatile bool button_press = FALSE;
-volatile bool button_1  = TRUE;
-volatile bool button_2 =TRUE;
-volatile bool previous_state =TRUE;
-static uint32_t msTicks = 0;
-
-
-/*..........................................................................................*/
-
-
-
-void SysTick_Handler(void)
-{
-    msTicks++;
-}
-
-
-static uint32_t getTicks(void)
-{
-    return msTicks;
-}
-
-/*...............................................................................................*/
-
-/* the related function read the value from pins 0.1 and 1.4, where the push buttons are connected
- * and return true or false in button_press variable if one of those buttons are pressed or not*/
-
-bool read_button_status()
-{
-
-	button_1 = GPIOGetValue (0, 1);
-	button_2 = GPIOGetValue (1, 4);
-
-	if((button_1==FALSE)||(button_2==FALSE))
-	{
-		button_press = TRUE;
-	}
-}
+volatile uint32_t previous_state ='C';
 
 /*................................................................................................*/
+
 /* temp_conversion function make the conversion of temperature unit from Celsius to Fahreinheit or
  * Fahreinheit to Celsius, depends on previous state, if a button is pressed */
+/*.............................................................................*/
 
+/*.............................................................................*/
 uint32_t temp_conversion (bool change_unit)
 
 {
-	int32_t t = 0;
-	int32_t t_unit = 0;
 
-	    /* Read temperature */
+	uint32_t t_unit=0;
 
-	        t = temp_read();
+	  /*  Read temperature  */
 
-	   if((change_unit == TRUE) && (previous_state == TRUE))
-	   {
-		   t_unit = (t * 9 / 5 + 32 ) * 10;
-             previous_state = FALSE;
-	   }
-       	if((change_unit == TRUE) && (previous_state == FALSE))
-	   {
-		   t_unit = t;
-		   previous_state = TRUE;
-	   }
+	switch(change_unit)
+	{
+		case  (1):
+		{
+			if (previous_state == 'C')
+				 {
+				           t_unit = temp_read();
+				           t_unit = t_unit / 10;
+						   t_unit = (t_unit * 9 / 5 + 32 );             /*  Celsius to Fahreinheit transformation; 0 degrees Celsius is equal to 32 degrees Fahrenheit
+						                                                The temperature T in degrees Fahrenheit (°F) is equal to the temperature T in degrees Celsius (°C) times 9/5 plus 32:  */
+				           previous_state = 'F';
+				           break;
+				 }
+	         if (previous_state == 'F')
+	        	 {
+						   t_unit = temp_read();
+						   t_unit = t_unit / 10;
+						   previous_state = 'C';
 
-       /* delay */
+						   break;
+	        	 }
 
-     delay32Ms(0, 200);
+		}
+		case (0):
+		{
+			if (previous_state == 'C')
+				{
+						   t_unit = temp_read();
+						   t_unit = t_unit / 10;
+				}
+			if (previous_state == 'F')
+				{
+					       t_unit = temp_read();
+					       t_unit = t_unit / 10;
+						   t_unit = (t_unit * 9 / 5 + 32 );
+				}
+		}
+		default  :
+			{
+				break;
+			}
+	}
 
 	   return t_unit;
+
 }
+
 /*.................................................................................................*/
 
-int main (void)
-{
-
-    GPIOInit();
-
-    init_timer32(0, 10);
-
-    temp_init(&getTicks);
-
-    /* setup sys Tick. Elapsed time is e.g. needed by temperature sensor */
-
-    SysTick_Config(SystemCoreClock / 1000);
-
-    if ( !(SysTick->CTRL & (1<<SysTick_CTRL_CLKSOURCE_Msk)) )
-    {
-
-      LPC_SYSCON->SYSTICKCLKDIV = 0x08;
-    }
-
-  while(1)
-  {
-	  read_button_status();
-	  temp_conversion (button_press);
-	  delay32Ms(0, 500);
-
-  }
-  }
 
